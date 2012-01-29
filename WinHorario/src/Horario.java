1b064831ec9c3c11f591f15d15390f4364c1ba4c
arturoserna <arturoserna@gmail.com>
2012-01-29 10:13:31 +0000

    Mejorado criterio de ordenacion: Ahora da preferencia a los que tienen algun espacio libre a medio dia para comer (entre las 13 y las 15.30)

--- WinHorario/src/Horario.java
+++ WinHorario/src/Horario.java
@@ -12,21 +12,21 @@
 public class Horario implements Comparable <Horario> {
     Clase[] clases;
     Dia[] dias;
-    int totaldias,peso,horasmuertas;
-    
-    double media,desviacion,mediasig,desvasig;
+    int totaldias,tiempoEnLaUni,horasmuertas;
+    int diasconlahoradelacomidalibre;
+    double mediaEntrada,desviacionHoraEntrada,mediasig,desvasig;
     
     Horario (Clase[] c){
         clases=c;
         totaldias=0;
-        peso=0;
+        tiempoEnLaUni=0;
         horasmuertas=0;
         
-        media=0; //sumatorio(dia[i].horafin-dia[i].horaini)/totaldias
-        desviacion=0;
+        mediaEntrada=0; //sumatorio(dia[i].horafin-dia[i].horaini)/totaldias
+        desviacionHoraEntrada=0;
         mediasig=0;
         desvasig=0;
-        
+        diasconlahoradelacomidalibre=0;
         dias = new Dia[7];
         for (int i=0; i<7;i++) dias[i]=new Dia();
         
@@ -39,9 +39,9 @@ public class Horario implements Comparable <Horario> {
         {
             if (dias[i].horasDeClase != 0)
             {
-                peso+=dias[i].tiempoEnLaUni;
+                tiempoEnLaUni+=dias[i].tiempoEnLaUni;
                 totaldias++;
-                media+=dias[i].fin-dias[i].ini;
+                mediaEntrada+=dias[i].ini;
                 mediasig+=dias[i].numasignaturas;
             }
             
@@ -50,19 +50,25 @@ public class Horario implements Comparable <Horario> {
                     dias[i].horasmuertas++;
                     horasmuertas++;
                 }
+                
+                for (int k=10; k <= 15; k++){
+                    if (!dias[i].ocupado[k]) dias[i].horapacomer=true;
+                }
+                
             }
         }
         
-        media/=totaldias;
+        mediaEntrada/=totaldias;
         mediasig/=totaldias;
         for (int i=0; i<7; i++){
             if (dias[i].horasDeClase!=0){
-                desviacion += (dias[i].fin-dias[i].ini-media)*(dias[i].fin-dias[i].ini-media);
+                desviacionHoraEntrada += (dias[i].ini-mediaEntrada)*(dias[i].ini-mediaEntrada);
                 desvasig += (dias[i].numasignaturas-mediasig)*(dias[i].numasignaturas-mediasig);
+                if (dias[i].horapacomer) diasconlahoradelacomidalibre++;
             }
         }
-        desviacion/= totaldias-1;
-        desviacion=Math.sqrt(desviacion);
+        desviacionHoraEntrada/= totaldias-1;
+        desviacionHoraEntrada=Math.sqrt(desviacionHoraEntrada);
         desvasig/= totaldias-1;
         desvasig=Math.sqrt(desvasig);
     }
@@ -71,28 +77,34 @@ public class Horario implements Comparable <Horario> {
     public int compareTo(Horario h) 
     { 
         int res = totaldias - h.totaldias;
+                
+        if (res==0) res= h.diasconlahoradelacomidalibre - diasconlahoradelacomidalibre;
         
-        if (res == 0) res = horasmuertas - h.horasmuertas;
+        if (res != 0) return res;
+        else
+        {
+            double aux = desviacionHoraEntrada - h.desviacionHoraEntrada;
+            
+             if (aux < 0) return -1;
+             else if (aux > 0) return 1;
+             else{
+                
         
-        if (res == 0) res = peso - h.peso;
+                res = horasmuertas - h.horasmuertas;
         
-        
-        return res;
-//        double res = desviacion - h.desviacion;
-        
-//        if      (res < 0) return -1;
-//        else if (res > 0) return  1;
-//        else {
-//            
-//            int r = horasmuertas - h.horasmuertas;
-//            
-//             if (r == 0) r = peso - h.peso;
-//        
-//             if (r == 0) r = totaldias - h.totaldias;
-//        
-//             return r;         
-//        }
-        
+                
+                if (res == 0) res = tiempoEnLaUni - h.tiempoEnLaUni;  
+                if (res == 0){
+                    aux=desvasig-h.desvasig;
+                    if      (aux < 0) return -1;
+                    else if (aux > 0) return 1;
+                    else return 0;
+                }
+                     
+            
+                return res;
+             }
+        }
     }
     
 }
